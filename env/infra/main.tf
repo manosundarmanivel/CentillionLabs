8f27512fa0f01e6ea0bf2ba9c88584d1b817f750
manosundarmanivel <manosundarmanivel@gmail.com>
2023-11-16 22:40:01 +0530

    .circleci test 1

--- env/infra/main.tf
+++ env/infra/main.tf
@@ -4,13 +4,13 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "tf-states-qa"
-    key    = "dev.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "tf-states-qa"
+#     key    = "dev.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
 
 module "network" {
   source = "../../modules/network"
@@ -51,20 +51,20 @@ module "network" {
 #   sub_pub = module.network.public-a-subnet-id
 # }
 
-module "kms" {
-  source = "../../modules/kms"
-  region = var.environment
-}
+# module "kms" {
+#   source = "../../modules/kms"
+#   region = var.environment
+# }
 
-module "sftp" {
-  source = "../../modules/sftp"
-  # depends_on = [module.storage.web_s3_01, module.network.vpc]
-  environment       = var.environment
-  short_region_name = var.short_region_name
-  vpc_id = module.network.vpc-id
-  sg_id = module.network.sg-id
-  s3_bucket = module.storage.web_s3_01.bucket
-}
+# module "sftp" {
+#   source = "../../modules/sftp"
+#   # depends_on = [module.storage.web_s3_01, module.network.vpc]
+#   environment       = var.environment
+#   short_region_name = var.short_region_name
+#   vpc_id = module.network.vpc-id
+#   sg_id = module.network.sg-id
+#   s3_bucket = module.storage.web_s3_01.bucket
+# }
 
 # module "beanstalk" {
 #   source = "../../modules/beanstalk"
